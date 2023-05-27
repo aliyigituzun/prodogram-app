e6a31471578cd83d93903aaeca49ea8fb00b7849
Ali Yiğit Uzun <yigituzun@Yigit-MacBook-Air-4.local>
2023-05-27 23:27:42 +0300

    =New

--- public/js/index/calculator.js
+++ public/js/index/calculator.js
@@ -4,11 +4,34 @@ window.addEventListener('load', () => {
     const tdeeButton = document.getElementById('tdee-button');
     const oneRepButton = document.getElementById('one-input');
 
-    tdeeResult = document.getElementById('tdee-result-text');
+    const tdeeResult = document.getElementById('tdee-result-text');
 
     document.addEventListener('click', (e) => {
 
-        if(e.target.id === 'bodyfat-button') {}
+        if(e.target.classList.contains('bodyfat-gender')) {
+            const selected = document.querySelector('.bodyfat-selected-gender');
+            if(selected) {
+                selected.classList.remove('bodyfat-selected-gender')
+            }
+            e.target.classList.add('bodyfat-selected-gender')
+        }
+        if(e.target.classList.contains('tdee-gender')) {
+            const selected = document.querySelector('.tdee-selected-gender');
+            if(selected) {
+                selected.classList.remove('tdee-selected-gender')
+            }
+            e.target.classList.add('tdee-selected-gender')
+        }
+        if(e.target.classList.contains('tdee-activity')) {
+            const selected = document.querySelector('.tdee-selected-activity');
+            if(selected) {
+                selected.classList.remove('tdee-selected-activity')
+            }
+            e.target.classList.add('tdee-selected-activity')
+        }
+        if(e.target.id === 'bodyfat-button') {
+
+        }
         if(e.target.id === 'one-rep-button') {
         }
         if(e.target.id === 'tdee-button') {
